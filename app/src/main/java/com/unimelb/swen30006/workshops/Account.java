fbbba0a8af9c2acf68ada2bdb23b56edd700004a
mpooley <mitchell.pooley@student.unimelb.edu.au>
2025-04-16 17:22:37 +1000

    Made Account Class

--- app/src/main/java/com/unimelb/swen30006/workshops/Account.java
+++ app/src/main/java/com/unimelb/swen30006/workshops/Account.java
@@ -1,5 +1,12 @@
 package com.unimelb.swen30006.workshops;
 
 public class Account {
-    public void onboard() {}
+    private State accountState = State.PENDING;
+    private float outStandingDebt = 0;
+    private DebtHealth debtHealth;
+    private boolean inGrace;
+
+    public void updateStatus(State state) {
+        accountState = state;
+    }
 }
