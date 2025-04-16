2701734041c92959b0e663ebfdcc1ed2d390cc9e
mpooley <mitchell.pooley@student.unimelb.edu.au>
2025-04-16 17:29:25 +1000

    Added a constructor and getters/setters for Account

--- app/src/main/java/com/unimelb/swen30006/workshops/Account.java
+++ app/src/main/java/com/unimelb/swen30006/workshops/Account.java
@@ -6,7 +6,21 @@ public class Account {
     private DebtHealth debtHealth;
     private boolean inGrace;
 
+    public Account() {}
+
     public void updateStatus(State state) {
         accountState = state;
     }
+
+    public State getAccountState() { return accountState;}
+    public void setAccountState(State accountState) { this.accountState = accountState;}
+
+    public float getOutStandingDebt() { return outStandingDebt;}
+    public void setOutStandingDebt(float outStandingDebt) { this.outStandingDebt = outStandingDebt;}
+
+    public DebtHealth getDebtHealth() { return debtHealth;}
+    public void setDebtHealth(DebtHealth debtHealth) { this.debtHealth = debtHealth;}
+
+    public boolean isInGrace() { return inGrace;}
+    public void setInGrace(boolean inGrace) { this.inGrace = inGrace;}
 }
