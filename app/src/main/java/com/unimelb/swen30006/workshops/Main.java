9dc2cb6699838237e0e123c3e777c6ae8268c532
Bec Chao <becchao@gmail.com>
2025-04-16 17:21:36 +1000

    Main

---
+++ app/src/main/java/com/unimelb/swen30006/workshops/Main.java
@@ -0,0 +1,11 @@
+package com.unimelb.swen30006.workshops;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class Main {
+	Account account = new Account();
+	public static void main(String[] args) {
+		System.out.println("Hi");
+	}
+}
